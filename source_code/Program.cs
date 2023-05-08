e4d2a3e5c2759efeda7032666b44a2d0ccc85d0d
roxxel <warperthe@gmail.com>
2023-05-08 12:03:11 +0300

    Refactoring

--- source_code/Program.cs
+++ source_code/Program.cs
@@ -1,303 +1,240 @@
-﻿using System.Diagnostics;
+﻿using Splash;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
 using System.Runtime.InteropServices;
+using static Splash.Native;
+class Program
+{
 
-class Program{
-     [Flags]
-    public enum ThreadAccess : int
+    private static Dictionary<string, Action<string, string>> _commandMap = new Dictionary<string, Action<string, string>>()
     {
-      TERMINATE = (0x0001),
-      SUSPEND_RESUME = (0x0002),
-      GET_CONTEXT = (0x0008),
-      SET_CONTEXT = (0x0010),
-      SET_INFORMATION = (0x0020),
-      QUERY_INFORMATION = (0x0040),
-      SET_THREAD_TOKEN = (0x0080),
-      IMPERSONATE = (0x0100),
-      DIRECT_IMPERSONATION = (0x0200)
-    }
-    static void Main(string[] args)
+        ["help"] = DisplayHelpMessage,
+        ["cls"] = ClearConsole,
+        ["clear"] = ClearConsole,
+        ["kill"] = KillProcess,
+        ["suspend"] = SuspendProcess,
+        ["unsuspend"] = UnsuspendProcess,
+        ["ps"] = ListProcesses,
+        ["find"] = FindProcess
+    };
+
+    
+    public static void Main(string[] args)
     {
-        [DllImport("kernel32.dll")]
-        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
-        [DllImport("kernel32.dll")]
-        static extern uint SuspendThread(IntPtr hThread);
-        [DllImport("kernel32.dll")]
-        static extern int ResumeThread(IntPtr hThread);
-        [DllImport("kernel32", CharSet = CharSet.Auto,SetLastError = true)]
-        static extern bool CloseHandle(IntPtr handle);
-
-        static void SuspendProcess(int pid)
-        {
-            var process = Process.GetProcessById(pid);
-
-            foreach (ProcessThread pT in process.Threads)
-            {
-                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
-
-                if (pOpenThread == IntPtr.Zero)
-                {
-                continue;
-                }
-
-                SuspendThread(pOpenThread);
-
-                CloseHandle(pOpenThread);
-            }
-        }
-
-        static void ResumeProcess(int pid)
-        {
-        var process = Process.GetProcessById(pid);
-
-        if (process.ProcessName == string.Empty)
-            return;
-
-            foreach (ProcessThread pT in process.Threads)
-            {
-                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
-
-                if (pOpenThread == IntPtr.Zero)
-                {
-                continue;
-                }
-
-                var suspendCount = 0;
-                do
-                {
-                suspendCount = ResumeThread(pOpenThread);
-                } while (suspendCount > 0);
-
-                CloseHandle(pOpenThread);
-            }
-        }
-
-        void cmd(string command){
-            System.Diagnostics.Process.Start("cmd.exe", "/C " + command);
-        }
-        cmd("cls");
         Console.Title = "Splash";
-        void splash(){
-            Thread.Sleep(300);
-            Console.Write("\n");
-            Console.WriteLine("#######################################################################################################");
-            Console.WriteLine("#  _####            _#######=_       ##                     ####                _####         ##      #");
-            Console.WriteLine("# ##-   #           #        #       ##                    ##  ##              ##-   #        ##      #");
-            Console.WriteLine("# ##                #        #       ##                   ##    ##             ##             #####   #");
-            Console.WriteLine("#   ###=_           #========J       ##                  ##      ##             ###=_         ######  #");
-            Console.WriteLine("#       ##          ##               ##                 ##========##                ##        ##   ## #");
-            Console.WriteLine("#       ##          #                ###               ##          ##               ##        ##   ## #");
-            Console.WriteLine("#  ######           #                #######          ##            ##         ######         ##   ## #");
-            Console.WriteLine("####################################################################################################### \n");
-        }
-        splash();
+
+        Utilities.SplashScreen();
         while (true)
         {
             Console.Write(">:>$>: ");
-            string? caa = Console.ReadLine();
-            string?[] cmnds = caa.Split("&");
-            foreach (string? comnd in cmnds)
+            string? input = Console.ReadLine();
+            string?[] commands = input.Split("&");
+            foreach (string? commandRaw in commands)
             {
-                string?[] caas = comnd.Split(" ");
-                string? cmnd = null;
-                string? a1 = null;
-                string? a2 = null;
+                string?[] commandParts = commandRaw.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string? command = null;
+                string? argument1 = null;
+                string? argument2 = null;
                 try
                 {
-                    cmnd = caas[0];
-                    a1 = caas[1];
-                    a2 = caas[2];
+                    command = commandParts[0];
+                    argument1 = commandParts[1];
+                    argument2 = commandParts[2];
                 }
-                catch{}
-                if (cmnd == "help")
+                catch { }
+                if (!_commandMap.TryGetValue(command, out var commandMethod))
                 {
-                    Console.WriteLine("help - show help");
-                    Console.WriteLine("clear or cls - cleans the console");
-                    Console.WriteLine("kill <process.name> or <process.pid>- kill process");
-                    Console.WriteLine("suspend <process.name> or <process.pid> - suspend process");
-                    Console.WriteLine("unsuspend <process.name> or <process.pid> - unsuspend process");
-                    Console.WriteLine("ps - write process list");
-                    Console.WriteLine("find <process.name> or <process.pid> - find process by pid or name");
+                    Console.WriteLine(command + " is an unknown command, if you need help type 'help' \n");
                 }
-                else if (cmnd == "clear")
+                else
                 {
-                    cmd("cls");
-                    splash();
-                }
-                else if (cmnd == "cls")
-                {
-                    cmd("cls");
-                    splash();
-                }
-                else if (cmnd == "kill")
-                {   
-                    if (a1 != null)
-                    {
-                        bool fon = false;
-                        foreach (var proc in Process.GetProcesses())
-                        {
-                            if (proc.Id.ToString() == a1)
-                            {
-                                proc.Kill();
-                                Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " killed");
-                                fon = true;
-                            }
-                            if (proc.ProcessName == a1)
-                            {
-                                proc.Kill();
-                                Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " killed");
-                                fon = true;
-                            }
-                        }
-                        if (fon == false)
-                        {
-                            Console.WriteLine(a1 + " not found");
-                        }
-                        Console.Write("\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You didn't pass an argument, use the `kill` command like this: `kill <process.name>` or `kill <process.pid>\n");
-                    }
-                }
-                else if (cmnd == "suspend")
-                {   
-                    if (a1 != null)
-                    {
-                        bool fon = false;
-                        foreach (var proc in Process.GetProcesses())
-                        {
-                            if (proc.Id.ToString() == a1)
-                            {
-                                SuspendProcess(Int32.Parse(a1));
-                                Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " suspended");
-                                fon = true;
-                            }
-                            if (proc.ProcessName == a1)
-                            {
-                                SuspendProcess(proc.Id);
-                                Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " suspended");
-                                fon = true;
-                            }
-                        }
-                        if (fon == false)
-                        {
-                            Console.WriteLine(a1 + " not found");
-                        }
-                        Console.Write("\n");
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("You didn't pass an argument, use the `suspend` command like this: `suspend <process.name>` or `suspend <process.pid>\n");
-                    }
-                }
-                else if (cmnd == "unsuspend")
-                {   
-                    if (a1 != null)
-                    {
-                        bool fon = false;
-                        foreach (var proc in Process.GetProcesses())
-                        {
-                            if (proc.Id.ToString() == a1)
-                            {
-                                ResumeProcess(Int32.Parse(a1));
-                                Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " unsuspended");
-                                fon = true;                        
-                            }
-                            if (proc.ProcessName == a1)
-                            {
-                                ResumeProcess(proc.Id);
-                                Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " unsuspended");
-                                fon = true;
-                            }
-                        }
-                        if (fon == false)
-                        {
-                            Console.WriteLine(a1 + " not found");
-                        }
-                        Console.Write("\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You didn't pass an argument, use the `unsuspend` command like this: `unsuspend <process.name>` or `unsuspend <process.pid> \n");
-                    }
-                }
-                else if (cmnd == "ps")
-                {
-                    var processes = Process.GetProcesses();
-
-					foreach (var process in processes)
-					{
-					    var isSuspended = false;
-
-					    foreach (ProcessThread thread in process.Threads)
-					    {
-					        if (thread.ThreadState == System.Diagnostics.ThreadState.Wait &&
-					            thread.WaitReason == ThreadWaitReason.Suspended)
-					        {
-					            isSuspended = true;
-					            break;
-					        }
-					    }
-
-					    if (isSuspended)
-					    {
-					        Console.WriteLine($"{process.Id}:::{process.ProcessName}:::Suspended");
-					    }
-					    else
-					    {
-					        Console.WriteLine($"{process.Id}:::{process.ProcessName}:::Working");
-					    }
-					}
-					Console.Write("\n");
-                }
-                else if (cmnd == "find")
-                {
-                    var processes = Process.GetProcesses();
-
-					foreach (var process in processes)
-					{
-					    var isSuspended = false;
-
-					    foreach (ProcessThread thread in process.Threads)
-					    {
-					        if (thread.ThreadState == System.Diagnostics.ThreadState.Wait &&
-					            thread.WaitReason == ThreadWaitReason.Suspended)
-					        {
-					            isSuspended = true;
-					            break;
-					        }
-					    }
-
-					    if (isSuspended)
-						{
-						    int id;
-						    if (int.TryParse(a1, out id) && process.Id == id || process.ProcessName == a1)
-						    {
-						        Console.WriteLine($"{process.Id}:::{process.ProcessName}:::Suspended");
-						    }
-						}
-						else
-						{
-						    int id;
-						    if (int.TryParse(a1, out id) && process.Id == id || process.ProcessName == a1)
-						    {
-						        Console.WriteLine($"{process.Id}:::{process.ProcessName}:::Working");
-						    }
-						}
-						
-					}
-					Console.Write("\n");
-                }
-                else if (cmnd == "")
-                {
-
-                }
-                else 
-                {
-                    Console.WriteLine(cmnd + " is an unknown command, if you need help type 'help' \n");
+                    commandMethod(argument1, argument2);
                 }
             }
         }
+
+    }
+
+    private static void FindProcess(string argument1, string _)
+    {
+        var processes = Process.GetProcesses();
+
+        foreach (var process in processes)
+        {
+            var isSuspended = false;
+
+            foreach (ProcessThread thread in process.Threads)
+            {
+                if (thread.ThreadState == System.Diagnostics.ThreadState.Wait &&
+                    thread.WaitReason == ThreadWaitReason.Suspended)
+                {
+                    isSuspended = true;
+                    break;
+                }
+            }
+
+            if (isSuspended)
+            {
+                int id;
+                if (int.TryParse(argument1, out id) && process.Id == id || process.ProcessName == argument1)
+                {
+                    Console.WriteLine($"{process.Id}:::{process.ProcessName}:::Suspended");
+                }
+            }
+            else
+            {
+                int id;
+                if (int.TryParse(argument1, out id) && process.Id == id || process.ProcessName == argument1)
+                {
+                    Console.WriteLine($"{process.Id}:::{process.ProcessName}:::Working");
+                }
+            }
+
+        }
+        Console.Write("\n");
+    }
+
+
+    private static void ClearConsole(string _, string __)
+    {
+        Console.Clear();
+        Utilities.SplashScreen();
+    }
+    private static void ListProcesses(string _, string __)
+    {
+        var processes = Process.GetProcesses();
+
+        foreach (var process in processes)
+        {
+            var isSuspended = false;
+
+            foreach (ProcessThread thread in process.Threads)
+            {
+                if (thread.ThreadState == System.Diagnostics.ThreadState.Wait &&
+                    thread.WaitReason == ThreadWaitReason.Suspended)
+                {
+                    isSuspended = true;
+                    break;
+                }
+            }
+
+            if (isSuspended)
+            {
+                Console.WriteLine($"{process.Id}:::{process.ProcessName}:::Suspended");
+            }
+            else
+            {
+                Console.WriteLine($"{process.Id}:::{process.ProcessName}:::Working");
+            }
+        }
+        Console.Write("\n");
+    }
+
+    private static void UnsuspendProcess(string argument1, string _)
+    {
+        if (argument1 != null)
+        {
+            bool fon = false;
+            foreach (var proc in Process.GetProcesses())
+            {
+                if (proc.Id.ToString() == argument1)
+                {
+                    Utilities.ResumeProcess(Int32.Parse(argument1));
+                    Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " unsuspended");
+                    fon = true;
+                }
+                if (proc.ProcessName == argument1)
+                {
+                    Utilities.ResumeProcess(proc.Id);
+                    Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " unsuspended");
+                    fon = true;
+                }
+            }
+            if (fon == false)
+            {
+                Console.WriteLine(argument1 + " not found");
+            }
+            Console.Write("\n");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass an argument, use the `unsuspend` command like this: `unsuspend <process.name>` or `unsuspend <process.pid> \n");
+        }
+    }
+
+    private static void SuspendProcess(string argument1, string _)
+    {
+        if (argument1 != null)
+        {
+            bool fon = false;
+            foreach (var proc in Process.GetProcesses())
+            {
+                if (proc.Id.ToString() == argument1)
+                {
+                    Utilities.SuspendProcess(Int32.Parse(argument1));
+                    Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " suspended");
+                    fon = true;
+                }
+                if (proc.ProcessName == argument1)
+                {
+                    Utilities.SuspendProcess(proc.Id);
+                    Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " suspended");
+                    fon = true;
+                }
+            }
+            if (fon == false)
+            {
+                Console.WriteLine(argument1 + " not found");
+            }
+            Console.Write("\n");
+        }
+
+        else
+        {
+            Console.WriteLine("You didn't pass an argument, use the `suspend` command like this: `suspend <process.name>` or `suspend <process.pid>\n");
+        }
+    }
+
+    private static void KillProcess(string argument1, string _)
+    {
+        if (argument1 != null)
+        {
+            bool fon = false;
+            foreach (var proc in Process.GetProcesses())
+            {
+                if (proc.Id.ToString() == argument1)
+                {
+                    proc.Kill();
+                    Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " killed");
+                    fon = true;
+                }
+                if (proc.ProcessName == argument1)
+                {
+                    proc.Kill();
+                    Console.WriteLine(proc.Id.ToString() + ":::" + proc.ProcessName + " killed");
+                    fon = true;
+                }
+            }
+            if (fon == false)
+            {
+                Console.WriteLine(argument1 + " not found");
+            }
+            Console.Write("\n");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass an argument, use the `kill` command like this: `kill <process.name>` or `kill <process.pid>\n");
+        }
+    }
+
+    private static void DisplayHelpMessage(string argument1, string argument2)
+    {
+        Console.WriteLine("help - show help");
+        Console.WriteLine("clear or cls - cleans the console");
+        Console.WriteLine("kill <process.name> or <process.pid>- kill process");
+        Console.WriteLine("suspend <process.name> or <process.pid> - suspend process");
+        Console.WriteLine("unsuspend <process.name> or <process.pid> - unsuspend process");
+        Console.WriteLine("ps - write process list");
+        Console.WriteLine("find <process.name> or <process.pid> - find process by pid or name");
     }
 }
