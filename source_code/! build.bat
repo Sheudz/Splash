15785ee62d2139b96c1f5d6efd1f7fecb3a5c179
Sheud <77610032+Sheudz@users.noreply.github.com>
2023-06-29 21:27:36 +0300

    Update builder

--- source_code/! build.bat
+++ source_code/! build.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Wait for build...
-dotnet publish
-del bin\Debug\net6.0\win-x86\publish\Splash.pdb
-start explorer.exe bin\Debug\net6.0\win-x86\publish
+dotnet publish --configuration Release
+del bin\Release\net6.0\win-x86\publish\Splash.pdb
+start explorer.exe bin\Release\net6.0\win-x86\publish
